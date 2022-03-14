e492c7e5bb806d28d1e8d4be0ddc4b5a271b6170
armanovxx <iebeat2018@outlook.com>
2022-03-14 11:23:45 +0100

    fixx bugs

--- client.js
+++ client.js
@@ -1,4 +1,3 @@
-import { operate } from "./module.js";
 const btns = document.querySelector(".compound-btns-container");
 const display = document.querySelector(".display");
 let op = false;
@@ -10,11 +9,56 @@ const dotFunc = () => {
         dot = true
     }
 }
+
+function operate(arr) {
+    let result = 0;
+    if (arr[0] === '') {
+        if (arr[1] == '+' || arr[1] == '-') {
+            arr.shift();
+            temp = arr[0];
+            arr.shift();
+            arr[0] = temp + arr[0];
+        }
+    }
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == "+") {
+            if (i == 1) {
+                result = parseFloat(arr[i - 1]) + parseFloat(arr[i + 1]);
+            } else {
+                result += parseFloat(arr[i + 1])
+            }
+        } else if (arr[i] == "-") {
+            if (i == 1) {
+                result = parseFloat(arr[i - 1]) - parseFloat(arr[i + 1]);
+            } else {
+                result -= parseFloat(arr[i + 1])
+            }
+        } else if (arr[i] == "*") {
+            if (i == 1) {
+                result = parseFloat(arr[i - 1]) * parseFloat(arr[i + 1]);
+            } else {
+                result *= parseFloat(arr[i + 1])
+            }
+        } else if (arr[i] == "/") {
+            if (arr[i + 1] == 0) {
+                alert("You can't divide by zero");
+            }
+            if (i == 1) {
+                result = parseFloat(arr[i - 1]) / parseFloat(arr[i + 1]);
+            } else {
+                result /= parseFloat(arr[i + 1])
+            }
+        }
+    }
+    return result
+}
 btns.addEventListener("click", (event) => {
     switch (event.target.value) {
         case 'res':
             display.textContent = '';
             dot = false;
+            op = false;
             break;
         case 'del':
             display.textContent = display.textContent.slice(0, -1);
@@ -25,9 +69,12 @@ btns.addEventListener("click", (event) => {
             dot = false;
             break;
         case '*':
-            if (!op) display.textContent += space + event.target.value + space;
-            op = "mul";
-            dot = false;
+            if (display.textContent != "") {
+                if (!op) display.textContent += space + event.target.value + space;
+                op = "mul";
+                dot = false;
+            }
+
             break;
         case '-':
             dot = false;
@@ -39,9 +86,12 @@ btns.addEventListener("click", (event) => {
             }
         case '+':
         case '/':
-            if (!op) display.textContent += space + event.target.value + space;
-            op = true;
-            dot = false;
+            if (display.textContent != '') {
+                if (!op) display.textContent += space + event.target.value + space;
+                op = true;
+                dot = false;
+            }
+
             break;
         default:
             if (event.target.value != undefined) {
